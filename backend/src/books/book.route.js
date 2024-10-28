21d009b6215d2c7925f85e3f17656999418d1cb2
khalidnbg <khalidnabgaoui4@gmail.com>
2024-10-28 15:03:14 +0100

    assign verification token to methods

--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -6,12 +6,13 @@ const {
   updateBook,
   deleteBook,
 } = require("./book.controller");
+const verifyAdminToken = require("../middleware/verifyAdminToken");
 const router = express.Router();
 
 // frontend => backend server => controller => book schema => database => sent to server => back to frontend => backend
 
 // add a book
-router.post("/create-book", postABook);
+router.post("/create-book", verifyAdminToken, postABook);
 
 // get all books
 router.get("/", getAllBooks);
@@ -20,9 +21,9 @@ router.get("/", getAllBooks);
 router.get("/:id", getSingleBook);
 
 // update a book
-router.put("/edit/:id", updateBook);
+router.put("/edit/:id", verifyAdminToken, updateBook);
 
 // delete a book
-router.delete("/delete/:id", deleteBook);
+router.delete("/delete/:id", verifyAdminToken, deleteBook);
 
 module.exports = router;
