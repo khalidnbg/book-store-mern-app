bf9c80e20484efa06b0c4e537508a5d61f5aaf92
khalidnbg <khalidnabgaoui4@gmail.com>
2024-10-29 15:22:18 +0100

    update book

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -14,6 +14,7 @@ import DashboardLayout from "../pages/dashboard/DashboardLayout";
 import Dashboard from "../pages/dashboard/Dashboard";
 import ManageBooks from "../pages/dashboard/manageBooks/ManagaBooks";
 import AddBook from "../pages/dashboard/addBook/AddBook";
+import UpdateBook from "../pages/dashboard/editBook/UpdateBook";
 
 const router = createBrowserRouter([
   {
@@ -96,8 +97,7 @@ const router = createBrowserRouter([
         path: "edit-book/:id",
         element: (
           <AdminRoute>
-            {/* <EditBook />, */}
-            <div>EditBook</div>
+            <UpdateBook />,
           </AdminRoute>
         ),
       },
